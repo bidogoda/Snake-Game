1c50e881f61c75d717b6b1ece61131a7aabc6713
AbdelrhmanGoda <103576472+bidogoda@users.noreply.github.com>
2026-06-25 22:38:29 +0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,48 @@
+import {
+  snakeSpeed,
+  update as updateSnake,
+  draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm("Game Over You Lost!!! Press Ok to restart.")) {
+      window.location = "/";
+    }
+    return;
+  }
+
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; // get seconds instaed of millisecond
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+  lastRenderTime = currentTime;
+  //   console.log(secondsSinceLastRender);
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
